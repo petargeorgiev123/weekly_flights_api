ba6fab53a37ab1dd6b0368b75dc4ef2909673b54
Petar Georgiev <petargeorgiev123@gmail.com>
2018-01-20 22:02:27 +0200

    API Change

--- src/AppBundle/Services/Scraper.php
+++ src/AppBundle/Services/Scraper.php
@@ -12,7 +12,7 @@ use projectivemotion\PhpScraperTools\CacheScraper;
 
 class Scraper extends CacheScraper
 {
-    const default_api_version = '7.7.5';
+    const default_api_version = '7.8.2';
     protected $protocol =   'https';
     protected $domain   =   'be.wizzair.com';
 
