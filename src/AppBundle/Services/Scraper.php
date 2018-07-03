06a4038c104c5e0d5986cad0bec8cb343bf9be8f
Petar Kirilov Georgiev <pkirilov@brte.boeing.es>
2018-07-03 10:31:49 +0200

    change api version

--- src/AppBundle/Services/Scraper.php
+++ src/AppBundle/Services/Scraper.php
@@ -12,7 +12,7 @@ use projectivemotion\PhpScraperTools\CacheScraper;
 
 class Scraper extends CacheScraper
 {
-    const default_api_version = '8.0.0';
+    const default_api_version = '8.1.0';
     protected $protocol =   'https';
     protected $domain   =   'be.wizzair.com';
 
